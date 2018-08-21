6b5132bad35943543dc99c1d619c9901d24dd45a
Jason Viger <jason.viger@gmail.com>
2018-08-21 08:27:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,8 @@ class Artist
 
   @@artists = []
 
-  #def self.find_by_name(name)
- #   @@artists.detect{|a| a.name == name}
- # end
-
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
@@ -33,8 +29,5 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
- # def to_param
- #   name.downcase.gsub(' ', '-')
- # end
 
 end
